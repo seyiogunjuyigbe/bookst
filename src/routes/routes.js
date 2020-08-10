0d0d477824dfe6e5c63a0d51b4b989e0207e80c3
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-08-10 20:27:38 +0100

    FE

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,12 @@
+let root = __dirname + "../../../public/html-demo/home";
 const authRoutes = require('./auth');
 const bookRoutes = require('./book');
 const initRoutes = app => {
     app.use('/auth', authRoutes);
-    app.use('/books', bookRoutes)
+    app.use('/books', bookRoutes);
+    app.get("/", (req, res) => {
+        return res.status(200).sendFile("index.html", { root })
+    })
     app.all("*", (req, res) => {
         return res.status(404).send('Sorry, requested route not found')
     })
